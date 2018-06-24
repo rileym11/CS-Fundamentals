62ec7cd09d6e3cc089261b364b8f9d6bb0659dd8
Riley MacDonald <32280362+rileym11@users.noreply.github.com>
2018-06-24 19:10:31 -0400

    Added base ES6 graph

--- Graphs/ES6Graph.js
+++ Graphs/ES6Graph.js
@@ -1,19 +1,20 @@
-const Dictionary = require('../Dictionaries(Maps)AndHashesTables/ES-5/Dictionary(Map)');
-
-function Graph() {
-  var vertices = []; //Array that will store all the vertices
-  var adjList = new Dictionary(); //ES5 map class to store the adjacent list
-
-  this.addVertex = function(v) {
-    vertices.push(v); //Push the vertex to the array of vertices
-    adjList.set(v, []); //Initialize a key with the new vertex and an empty adjacent array as the value
-  };
-  this.addEdge = function(v, w) {
-    adjList.get(v).push(w); //Add the w vertex into the v vertex's adjacent list
-    adjList.get(w).push(v); //If using Un-directed graphs then push v vertex into the w vertex's adjacent list
-  };
-  this.toString = function() {
+class ES6Graph {
+  constructor() {
+    this.vertices = []; //Array that will store all the vertices
+    this.adjList = new Map(); //ES6 map class to store the adjacent list
+  }
+  addVertex(v) {
+    this.vertices.push(v); //Push the vertex to the array of vertices
+    this.adjList.set(v, []); //Initialize a key with the new vertex and an empty adjacent array as the value
+  }
+  addEdge(v, w) {
+    this.adjList.get(v).push(w); //Add the w vertex into the v vertex's adjacent list
+    this.adjList.get(w).push(v); //If using Un-directed graphs then push v vertex into the w vertex's adjacent list
+  }
+  toString() {
     var s = '';
+    let vertices = this.vertices;
+    let adjList = this.adjList;
     for (var i = 0; i < vertices.length; i++) {
       //Iterate through all the vertices
       s += vertices[i] + ' -> ';
@@ -25,11 +26,11 @@ function Graph() {
       s += '\n'; //so each vertex is printed on a new line
     }
     return s;
-  };
+  }
 }
 
 //Tests
-var graph = new Graph();
+var graph = new ES6Graph();
 var myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
 for (var i = 0; i < myVertices.length; i++) {
   //Add all the vertices
